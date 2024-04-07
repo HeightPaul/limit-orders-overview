6c4c398d565bb11e497b4cd224ea87e42a9bd41d
HeightPaul <hristopaul@gmail.com>
2024-04-07 09:05:30 +0300

    Filter together coin symbol and name

--- src/utils/coingecko.js
+++ src/utils/coingecko.js
@@ -13,17 +13,13 @@ async function searchCoinIds(tokens) {
       }
 
       const searchTokens = tokens.filter(token => token.symbol !== ERROR_MSG)
-
       for (const searchToken of searchTokens) {
-         const matchingCoinsBySymbol = coins.filter(coin => coin.symbol.toLowerCase() === searchToken.symbol.toLowerCase())
-         if (!matchingCoinsBySymbol[0]?.id) continue
-
-         if (matchingCoinsBySymbol.length > 1) {
-            const matchingCoinsByName = matchingCoinsBySymbol.filter(coin => coin.name.includes(searchToken.name) || searchToken.name.includes(coin.name))
-
-            coinIds.push(matchingCoinsByName[0]?.id || matchingCoinsBySymbol[0].id)
-         } else {
-            coinIds.push(matchingCoinsBySymbol[0].id)
+         const matchingCoins = coins.filter(
+            coin => coin.symbol.toLowerCase() === searchToken.symbol.toLowerCase()
+            && (coin.name.includes(searchToken.name) || searchToken.name.includes(coin.name))
+         )
+         if (matchingCoins[0]?.id) {
+            coinIds.push(matchingCoins[0].id)
          }
       }
 
